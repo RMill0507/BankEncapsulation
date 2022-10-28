226a17f6c4f5c29601fd92a0182a2561a6b16cc5
Rob <Rmilla445@gmail.com>
2022-10-28 13:39:06 -0400

    Bank Account info

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    internal class BankAccount
+    {
+        public BankAccount()
+        {
+
+        }
+
+        private double _balence = 0;
+
+        public double Deposit(double amount)
+        {
+            return _balence += amount;
+        }
+        public double Withdraw(double amount)
+        {
+            return _balence -= amount;
+        }
+        public double GetBalance()
+        {
+            return _balence;
+        }
+
+
+
+
+
+
+
+    }
+    
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -2,9 +2,19 @@
 {
     public class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount yourAccount = new BankAccount();
+
+            Console.WriteLine($"You have ${yourAccount.GetBalance()} haha lol");
+            yourAccount.Deposit(500);
+            Console.WriteLine($"You just put in ${yourAccount.GetBalance()}. Glad your getting some money...:)");
+
+
+
+
+
         }
     }
 }
